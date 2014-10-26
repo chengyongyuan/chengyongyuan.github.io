8bed69b66219984f9d637e9052ff95124d64c4c8
chengyongyuan <334089103@qq.com>
2014-10-26 02:25:37 -0700

    1. modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # delpy hugo powered site on github
 
-hugo -b "http://chengyongyuan.github.com/public" -t hyge
+hugo -b "http://chengyongyuan.github.com/public" -t hyde
 
-git mv public/index.html .
+mv public/index.html .
 
 git add index.html  config.toml deploy.sh
 
